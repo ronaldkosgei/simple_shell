e419c1ac03f0d8c1638e886743f73a2a067ca808
ronaldkosgei <kosgeironald4@gmail.com>
2023-10-21 16:36:02 +0300

    Swaps int

---
+++ Swap_int.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * swap_int - This function swaps values.
+ * @a: This is a param to be reset.
+ * @b: second value;
+ */
+void swap_int(int a, int b)
+{
+int x;
+x = a;
+a = b;
+b = x;
+}

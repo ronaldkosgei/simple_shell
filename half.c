7b1ad08afb1ad7345808f3f17581eceb00ff6add
ronaldkosgei <kosgeironald4@gmail.com>
2023-10-21 15:44:15 +0300

    print half

---
+++ half.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+/**
+ * prints_half - This function prints the second half of a string.
+ * @str: This is a parameter.
+ */
+void prints_half(char *str)
+{
+int l;
+l = 0;
+while (str[l] != '\0')
+{
+l++;
+}
+}
